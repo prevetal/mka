8ad58b7d1402cc3a07f9d129ca2a54e72a52986a
Vitaliy Sulimenko <prevetal@gmail.com>
2025-02-05 18:04:30 +0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,266 @@
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	// Main slider
+	let mainSlider = document.querySelector('.main_slider .swiper')
+
+	if (mainSlider) {
+		new Swiper('.main_slider .swiper', {
+			loop: true,
+			speed: 750,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			lazy: true
+		})
+	}
+
+
+	// Products slider
+	const productsSliders = [],
+		products = document.querySelectorAll('.products .swiper')
+
+	products.forEach((el, i) => {
+		el.classList.add('products_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			lazy: true,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 12,
+					slidesPerView: 'auto'
+				},
+				480: {
+					spaceBetween: 20,
+					slidesPerView: 2
+				},
+				768: {
+					spaceBetween: 30,
+					slidesPerView: 3
+				},
+				1280: {
+					spaceBetween: 30,
+					slidesPerView: 4
+				}
+			},
+			on: {
+				init: swiper => setHeight(swiper.el.querySelectorAll('.product')),
+				resize: swiper => {
+					let products = swiper.el.querySelectorAll('.product')
+
+					products.forEach(el => el.style.height = 'auto')
+
+					setHeight(products)
+				}
+			}
+		}
+
+		productsSliders.push(new Swiper('.products_s' + i, options))
+	})
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.trapFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.placeFocusBack = false
+	Fancybox.defaults.l10n = {
+		CLOSE: 'Закрыть',
+		NEXT: 'Следующий',
+		PREV: 'Предыдущий',
+		MODAL: 'Вы можете закрыть это модальное окно нажав клавишу ESC'
+	}
+
+	Fancybox.defaults.tpl = {
+		closeButton: '<button data-fancybox-close class="f-button is-close-btn" title="{{CLOSE}}"><svg><use xlink:href="images/sprite.svg#ic_close"></use></svg></button>',
+
+		main: `<div class="fancybox__container" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1">
+			<div class="fancybox__backdrop"></div>
+			<div class="fancybox__carousel"></div>
+			<div class="fancybox__footer"></div>
+		</div>`,
+	}
+
+
+	// Modals
+	$('.modal_btn').click(function(e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: document.getElementById(e.target.getAttribute('data-modal')),
+			type: 'inline'
+		}])
+	})
+
+
+	// Zoom images
+	Fancybox.bind('.fancy_img', {
+		Image: {
+			zoom: false
+		},
+		Thumbs: {
+			autoStart: false
+		}
+	})
+
+
+	// Mob. menu
+	$('.mob_header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('.mob_header .mob_menu_btn').toggleClass('active')
+		$('body').toggleClass('lock')
+		$('header').toggleClass('show')
+
+		$('.mob_header .mob_menu_btn').hasClass('active')
+			? $('.overlay').fadeIn(300)
+			: $('.overlay').fadeOut(300)
+	})
+
+
+	// 'Up' button
+	$('.buttonUp .btn').click((e) => {
+		e.preventDefault()
+
+		$('body, html').stop(false, false).animate({ scrollTop: 0 }, 1000)
+	})
+
+
+	// Phone input mask
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+
+
+	// Focus when clicking on the field name
+	const formLabels = document.querySelectorAll('form .label')
+
+	if (formLabels) {
+		formLabels.forEach(el => {
+			el.addEventListener('click', e => {
+				e.preventDefault()
+
+				el.closest('.line').querySelector('.input, textarea').focus()
+			})
+		})
+	}
+
+
+	// Select file
+	const fileInputs = document.querySelectorAll('form input[type=file]')
+
+	if (fileInputs) {
+		fileInputs.forEach(el => {
+			el.addEventListener('change', () => el.closest('.file').querySelector('label span').innerText = el.value)
+		})
+	}
+
+
+	if (is_touch_device()) {
+		const subMenus = document.querySelectorAll('header .menu .sub_menu')
+
+		// Submenu on the touch screen
+		$('header .menu_item > a.sub_link').addClass('touch_link')
+
+		$('header .menu_item > a.sub_link').click(function (e) {
+			const dropdown = $(this).next()
+
+			if (dropdown.css('visibility') === 'hidden') {
+				e.preventDefault()
+
+				subMenus.forEach(el => el.classList.remove('show'))
+				dropdown.addClass('show')
+
+				BODY.style = 'cursor: pointer;'
+			}
+		})
+
+		// Close the submenu when clicking outside it
+		document.addEventListener('click', e => {
+			if ($(e.target).closest('.menu').length === 0) {
+				subMenus.forEach(el => el.classList.remove('show'))
+
+				BODY.style = 'cursor: default;'
+			}
+		})
+	}
+})
+
+
+
+window.addEventListener('scroll', function () {
+	// 'Up' button
+	$(window).scrollTop() > $(window).innerHeight()
+		? $('.buttonUp').fadeIn(300)
+		: $('.buttonUp').fadeOut(200)
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || BODY.clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Overwrite window width
+		WW = window.innerWidth || document.clientWidth || BODY.clientWidth
+
+
+		// Mob. version
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+	}
+})
